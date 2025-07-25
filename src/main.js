4034843ad57c4d3045271253810bdf752b45eced
Vladislav <v.suvorkin2017@gmail.com>
2025-07-25 17:35:23 +0300

    Ready-made full page layout, as well as all styles (so far without adaptability)

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "./css/main.css";
+import "./css/resume-container.css";
+
+import "./css/components/avatar.css";
+import "./css/components/header.css";
+import "./css/components/languages.css";
+import "./css/components/experience.css";
+import "./css/components/tools.css";
+import "./css/components/education.css";
+import "./css/components/interests.css";
+import "./css/components/contacts.css";
+import "./css/components/download-button.css";
+
+import "./css/editable.css";
+import "./css/animations.css";
+import "./css/media.css";
